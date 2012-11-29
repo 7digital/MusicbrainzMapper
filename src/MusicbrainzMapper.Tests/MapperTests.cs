60bd1dedc41465357015fd897a98a91889b0a267
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-11-29 15:07:08 +0000

    first tests and high level interface specified

---
+++ src/MusicbrainzMapper.Tests/MapperTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace MusicbrainzMapper.Tests
+{
+    [TestFixture]
+    public class MapperTests
+    {
+        [Test]
+        public void WhenMappingA7digitalReleaseidIGetAListOfMusicbrainzids()
+        {
+            var mapper = new SevenDigitalToMusicBrainzMapper();
+            const int sevenDigitalReleaseId = 12345;
+
+            var result = mapper.MapAsync(sevenDigitalReleaseId);
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
---
+++ src/MusicbrainzMapper/SevenDigitalToMusicBrainzMapper.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicbrainzMapper
+{
+    public class SevenDigitalToMusicBrainzMapper
+    {
+        public async Task<IList<int>> MapAsync(int idToMap)
+        {
+            return new List<int>();
+        }
+    }
+}
