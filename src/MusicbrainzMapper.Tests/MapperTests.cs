4efdc2ffb3e58df40a76541bc0a23d75ec8f3bdc
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-11-29 15:46:42 +0000

    refactor tests

--- src/MusicbrainzMapper.Tests/MapperTests.cs
+++ src/MusicbrainzMapper.Tests/MapperTests.cs
@@ -6,16 +6,24 @@ namespace MusicbrainzMapper.Tests
     [TestFixture]
     public class MapperTests
     {
+        private const int SevenDigitalReleaseId = 12345;
+        private ITrackDurationService _trackDurationService;
+        private SevenDigitalToMusicBrainzMapper _mapper;
+
+        [SetUp]
+        public void CreateMapper()
+        {
+            _trackDurationService = Substitute.For<ITrackDurationService>();
+
+            _mapper = new SevenDigitalToMusicBrainzMapper(_trackDurationService);
+        }
+
         [Test]
         public void WhenMappingA7digitalReleaseidIGetAListOfMusicbrainzids()
         {
-            var trackDurationService = Substitute.For<ITrackDurationService>();
-
-            var mapper = new SevenDigitalToMusicBrainzMapper(trackDurationService);
-            const int sevenDigitalReleaseId = 12345;
-
-            var result = mapper.MapAsync(sevenDigitalReleaseId);
+            var result = _mapper.MapAsync(SevenDigitalReleaseId);
             Assert.That(result, Is.Not.Null);
         }
+
     }
 }
