5b988ea05275047f4814029c3618d881ecca2d64
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-11-29 17:54:00 +0000

    created matcher

---
+++ src/MusicbrainzMapper.Tests/TrackDurationMatcherTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MusicbrainzMapper.Tests
+{
+    [TestFixture]
+    public class TrackDurationMatcherTests
+    {
+        [Test]
+        public async void TrackDurationMatcherFetchesNonNullForNonNullInput()
+        {
+            var matcher = new TrackDurationMatcher();
+            var matches = await matcher.FindMatchesAsync(new List<int>());
+
+            Assert.That(matches, Is.Not.Null);
+        }
+    }
+}
---
+++ src/MusicbrainzMapper/TrackDurationMatcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MusicbrainzMapper
+{
+    public class TrackDurationMatcher: ITrackDurationMatcher
+    {
+        public async Task<IList<string>> FindMatchesAsync(IList<int> trackDuration)
+        {
+            return new List<string>();
+        }
+    }
+}
