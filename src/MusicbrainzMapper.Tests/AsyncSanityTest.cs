7f148f76748bf216164b63057f7c97ff0ce4adf1
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-05 10:53:43 +0000

    extract method

--- src/MusicbrainzMapper.Tests/AsyncSanityTest.cs
+++ src/MusicbrainzMapper.Tests/AsyncSanityTest.cs
@@ -6,8 +6,21 @@ namespace MusicbrainzMapper.Tests
 {
     public abstract class AsyncSanityTest
     {
+        public class TaskTiming
+        {
+            public long CreationTime;
+            public long CompletionTime;
+        }
+
         [Test, Explicit]
         public async void TaskCreationIsQuickerThanExecution()
+        {
+            var taskTiming = await GetTaskTiming();
+
+            Assert.That(taskTiming.CreationTime, Is.LessThan(taskTiming.CompletionTime));
+        }
+
+        public async Task<TaskTiming> GetTaskTiming()
         {
             var stopwatch = Stopwatch.StartNew();
 
@@ -19,7 +32,11 @@ namespace MusicbrainzMapper.Tests
 
             var taskCompletionTime = taskCreationAndCompletionTime - taskCreationTime;
 
-            Assert.That(taskCreationTime, Is.LessThan(taskCompletionTime));
+            return new TaskTiming
+                       {
+                           CreationTime = taskCreationTime,
+                           CompletionTime = taskCompletionTime
+                       };
         }
 
         protected abstract Task ToTest();
