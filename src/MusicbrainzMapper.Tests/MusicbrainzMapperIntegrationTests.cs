2ddd78a0183c574a54c50caf32913e6419229a1b
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-14 12:04:15 +0000

    added fixture setup

--- src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
+++ src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
@@ -6,20 +6,24 @@ namespace MusicbrainzMapper.Tests
     [TestFixture]
     public class MusicbrainzMapperIntegrationTests
     {
+        private SevenDigitalToMusicBrainzMapper _mapper;
         private const string ConnectionString = "Server=10.0.10.119;Port=5432;User Id=musicbrainz;Password=musicbrainz;Database=musicbrainz_db;";
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            var matcher = new TrackDurationMatcher(ConnectionString);
+            var durationService = new TrackDurationService();
+            _mapper = new SevenDigitalToMusicBrainzMapper(durationService, matcher);
+        }
 
         [TestCase(287887, "e956c901-acb7-48d6-9dc6-389a5f91f372")]
         public void Maps7dReleaseIdtoMbId(int sevenDigitalId, string musicBrainzId)
         {
             var mbId = new Guid(musicBrainzId);
-            var matcher = new TrackDurationMatcher(ConnectionString);
-            var durationService = new TrackDurationService();
-            var mapper = new SevenDigitalToMusicBrainzMapper(durationService, matcher);
-
-            var matches = mapper.Map(sevenDigitalId);
+            var matches = _mapper.Map(sevenDigitalId);
 
             Assert.That(matches, Contains.Item(mbId));
         }
-
     }
 }
