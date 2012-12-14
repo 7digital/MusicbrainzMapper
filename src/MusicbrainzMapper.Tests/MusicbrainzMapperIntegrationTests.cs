b1b0a9f6ea873b17c08671d3962ba8cc23a3171f
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-14 12:20:30 +0000

    another testcase
    Conflicts:
    
    	MusicbrainzMapper.sln.DotSettings.user
    	src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs

--- src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
+++ src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
@@ -19,6 +19,7 @@ namespace MusicbrainzMapper.Tests
 
         [TestCase(287887, "e956c901-acb7-48d6-9dc6-389a5f91f372", "Rage Against The Machine")]
         [TestCase(1076864, "c51eb64c-e4e2-3992-8f4e-49909853a663", "21")]
+        [TestCase(499472, "3fed3ec7-96da-497e-989f-0b9e711378d2", "Emigrantski Raggamuffin")]
         public async void Maps7dReleaseIdtoMbId(int sevenDigitalId, string musicBrainzId, string releaseName)
         {
             var mbId = new Guid(musicBrainzId);
