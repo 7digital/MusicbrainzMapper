70b13584da6fb79f12c55f75fa354c5e63c6b7c6
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-14 12:31:21 +0000

    another testcase

--- src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
+++ src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
@@ -20,6 +20,7 @@ namespace MusicbrainzMapper.Tests
         [TestCase(287887, "e956c901-acb7-48d6-9dc6-389a5f91f372", "Rage Against The Machine")]
         [TestCase(1076864, "c51eb64c-e4e2-3992-8f4e-49909853a663", "21")]
         [TestCase(499472, "3fed3ec7-96da-497e-989f-0b9e711378d2", "Emigrantski Raggamuffin")]
+        [TestCase(28815, "497f9acf-a695-332e-bc20-1c8745248550", "Homework")]
         public void Maps7dReleaseIdtoMbId(int sevenDigitalId, string musicBrainzId, string releaseName)
         {
             var mbId = new Guid(musicBrainzId);
