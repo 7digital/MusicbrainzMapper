d5b8aad758ac5cf12a1afe9c43bf02ae2ecfede1
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-14 12:08:33 +0000

    add name of release

--- src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
+++ src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
@@ -17,13 +17,13 @@ namespace MusicbrainzMapper.Tests
             _mapper = new SevenDigitalToMusicBrainzMapper(durationService, matcher);
         }
 
-        [TestCase(287887, "e956c901-acb7-48d6-9dc6-389a5f91f372")]
-        public void Maps7dReleaseIdtoMbId(int sevenDigitalId, string musicBrainzId)
+        [TestCase(287887, "e956c901-acb7-48d6-9dc6-389a5f91f372", "Rage Against The Machine")]
+        public void Maps7dReleaseIdtoMbId(int sevenDigitalId, string musicBrainzId, string releaseName)
         {
             var mbId = new Guid(musicBrainzId);
             var matches = _mapper.Map(sevenDigitalId);
 
-            Assert.That(matches, Contains.Item(mbId));
+            Assert.That(matches, Contains.Item(mbId), string.Format("Could not map {0}", releaseName));
         }
     }
 }
