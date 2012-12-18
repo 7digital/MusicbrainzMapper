5a5f8a5993206b49ceb60f4432abcf5831e9a6f1
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-18 17:38:14 +0000

    code to check data

--- src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
+++ src/MusicbrainzMapper.Tests/MusicbrainzMapperIntegrationTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 using NUnit.Framework;
+using SevenDigital.Api.Wrapper.Exceptions;
 
 namespace MusicbrainzMapper.Tests
 {
@@ -8,13 +12,14 @@ namespace MusicbrainzMapper.Tests
     {
         private SevenDigitalToMusicBrainzMapper _mapper;
         private MusicbrainzTrackDurationMatcher _trackDurationMatcher;
+        private SevenDigitalTrackDurationService _trackDurationService;
 
         [TestFixtureSetUp]
         public void SetUp()
         {
-            var durationService = new SevenDigitalTrackDurationService();
+            _trackDurationService = new SevenDigitalTrackDurationService();
             _trackDurationMatcher = new MusicbrainzTrackDurationMatcher();
-            _mapper = new SevenDigitalToMusicBrainzMapper(durationService, _trackDurationMatcher);
+            _mapper = new SevenDigitalToMusicBrainzMapper(_trackDurationService, _trackDurationMatcher);
         }
 
         [TestCase(287887, "e956c901-acb7-48d6-9dc6-389a5f91f372", "Rage Against The Machine")]
@@ -27,6 +32,63 @@ namespace MusicbrainzMapper.Tests
             var matches = _mapper.Map(sevenDigitalId);
 
             Assert.That(matches, Contains.Item(mbId), string.Format("Could not map {0}", releaseName));
+        }
+
+        [Test, Explicit]
+        public void CheckLastFMData()
+        {
+            var result = File.Create(@"../../../../no_matches.tsv");
+            using (var writer = new StreamWriter(result, Encoding.UTF8))
+            {
+                writer.Write("7digital release id");
+                writer.Write("\t");
+                writer.Write("Musicbrainz id");
+                writer.Write("\t");
+                writer.Write("matches found");
+                writer.Write("\t");
+                writer.Write("Ids match");
+                writer.Write('\n');
+
+                foreach (var line in File.ReadAllLines(@"../../../../albums.query.tsv").Take(100))
+                {
+                    var split = line.Split('\t');
+                    var sdId = int.Parse(split[1]);
+                    var mbId = new Guid(split[0]);
+
+                    var country = split[2].Substring(7, 3);
+
+                    _trackDurationService.Country = country.EndsWith(".")
+                                                        ? country.Substring(0, 2)
+                                                        : "GB";
+                    writer.Write(sdId);
+                    writer.Write('\t');
+                    writer.Write(mbId);
+                    writer.Write('\t');
+
+                    try
+                    {
+                        var matches = _mapper.Map(sdId).Count;
+                        writer.Write(matches);
+                        writer.Write('\t');
+
+                        if (_mapper.Map(sdId).Contains(mbId))
+                        {
+                            writer.Write("match");
+                        }
+                        else
+                        {
+                            writer.Write("no match");
+                        }
+                    }
+                    catch (InvalidResourceException)
+                    {
+                        writer.Write(0);
+                        writer.Write('\t');
+                        writer.Write("no release found");
+                    }
+                    writer.Write('\n');
+                }
+            }
         }
 
         [TestFixtureTearDown]
--- src/MusicbrainzMapper/SevenDigitalTrackDurationService.cs
+++ src/MusicbrainzMapper/SevenDigitalTrackDurationService.cs
@@ -7,10 +7,22 @@ namespace MusicbrainzMapper
 {
     public class SevenDigitalTrackDurationService : ITrackDurationService<int>
     {
+        public string Country;
+
+        public SevenDigitalTrackDurationService() : this("GB")
+        {
+        }
+
+        public SevenDigitalTrackDurationService(string country)
+        {
+            Country = country;
+        }
+
         public IList<int> Get(int releaseId)
         {
             return Api<ReleaseTracks>.Create
                                      .ForReleaseId(releaseId)
+                                     .WithParameter("country", Country)
                                      .Please()
                                      .Tracks
                                      .Select(track => ConvertSecondsToMilliseconds(track.Duration))
